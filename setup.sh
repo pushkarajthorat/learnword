c0d37553802d22ec176acbfc8d42d94831db6bcf
Pushkaraj Thorat <pushkarajthorat@gmail.com>
2014-08-24 19:03:19 +0530

    some more changes (don't know)

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-for file in `cat BarronsWordList.txt`
+for file in `cat today`
+#for file in acerbity
 do
-	mkdir $file;
-	wget http://www.oxforddictionaries.com/definition/english/$file -O oxford-dict/$file/$file
-	python FormatWordPage.py $file/$file | tr '\n' ' ' | sed -e 's/<header class/\n<header class/g' -e 's/<\/header>/<\/header>\n/g' -e 's/<\/h2>/<\/h2>\n/g' -e 's/<h2/\n<h2/g'  | grep -v '</div>/</div></header>$' | grep -v '^<header class="entryHeader">' | tr '\n' ' ' | sed -e 's/<h2> Definition of <strong>/\n<h2> Definition of <strong>/g' -e 's/<\/ul>/<\/ul>\n/g'  | grep -v "^<h2> Definition of <strong>" > oxford-dict/$file/meaning
-	cd ..
+    [ -d oxford-dict/"$file" ] && continue
+	mkdir oxford-dict/"$file";
+	#wget http://www.oxforddictionaries.com/definition/english/$file -O oxford-dict/$file/$file
+	wget "http://www.oxforddictionaries.com/search/english/?direct=1&multi=1&q=$file" -O oxford-dict/$file/$file
+	python FormatWordPage.py oxford-dict/"$file/$file" | tr '\n' ' ' | sed -e 's/<header class/\n<header class/g' -e 's/<\/header>/<\/header>\n/g' -e 's/<\/h2>/<\/h2>\n/g' -e 's/<h2/\n<h2/g'  | grep -v '</div>/</div></header>$' | grep -v '^<header class="entryHeader">' | tr '\n' ' ' | sed -e 's/<h2> Definition of <strong>/\n<h2> Definition of <strong>/g' -e 's/<\/ul>/<\/ul>\n/g'  | grep -v "^<h2> Definition of <strong>" > oxford-dict/"$file"/meaning
 done;
 
 
